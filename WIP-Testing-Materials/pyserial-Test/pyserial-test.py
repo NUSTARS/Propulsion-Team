21234efd2372993376bf9d0faba66bb532b682fb
burkeastanton <burkeastanton@gmail.com>
2025-11-09 19:36:40 -0600

    rs485/422 is kinda working

--- WIP-Testing-Materials/pyserial-Test/pyserial-test.py
+++ WIP-Testing-Materials/pyserial-Test/pyserial-test.py
@@ -3,11 +3,25 @@
 import serial
 import time
 
-ser = serial.Serial('COM3', baudrate = 115200)
+ser = serial.Serial('COM3', baudrate = 9600)
+
+sending = True
 
 while True:
-    if ser.in_waiting > 0:
-        data = ser.readline().decode('utf-8').strip()
-        print(f"Recieved: {data}")
+    
+    if not sending:
         
-    time.sleep(0.1)
+        if (ser.in_waiting > 7):
+            print(f"ser in waiting {ser.in_waiting}\n")
+            data = ser.read(ser.in_waiting).decode('utf-8')
+            print(f"Recieved: {data}")
+            #time.sleep(0.5)
+            sending = True
+    else:   
+        data_to_send = input("input: ")  
+        print(f"{data_to_send}")
+        ser.write(data_to_send.encode('utf-8'))
+        sending = False
+        time.sleep(.01)
+        
+    #time.sleep(0.5)

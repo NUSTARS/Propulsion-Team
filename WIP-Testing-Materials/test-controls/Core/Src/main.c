18ae74b328e93fcddce1ba4fdde9e5e5ac583eda
okrastew-2 <omarkamil2027@u.northwestern.edu>
2026-02-28 14:04:12 -0600

    Update

--- WIP-Testing-Materials/test-controls/Core/Src/main.c
+++ WIP-Testing-Materials/test-controls/Core/Src/main.c
@@ -160,6 +160,7 @@ void get_pressure_data(){
 	pressure_transducer_outputs.values.res3 = ADC_Convert(&hadc1);
 	pressure_transducer_outputs.values.res4 = 0;
 	pressure_transducer_outputs.values.res5 = 0;
+	HAL_ADC_Stop(&hadc1);
 
 
 //	for (int i = 0; i < 10; i ++){
@@ -613,9 +614,9 @@ static void MX_ADC1_Init(void)
 
   /** Configure Regular Channel
   */
-  sConfig.Channel = ADC_CHANNEL_10;
+  sConfig.Channel = ADC_CHANNEL_5;
   sConfig.Rank = ADC_REGULAR_RANK_1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
+  sConfig.SamplingTime = ADC_SAMPLETIME_32CYCLES_5;
   sConfig.SingleDiff = ADC_SINGLE_ENDED;
   sConfig.OffsetNumber = ADC_OFFSET_NONE;
   sConfig.Offset = 0;
@@ -627,6 +628,7 @@ static void MX_ADC1_Init(void)
 
   /** Configure Regular Channel
   */
+  sConfig.Channel = ADC_CHANNEL_10;
   sConfig.Rank = ADC_REGULAR_RANK_2;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
@@ -635,6 +637,7 @@ static void MX_ADC1_Init(void)
 
   /** Configure Regular Channel
   */
+  sConfig.Channel = ADC_CHANNEL_15;
   sConfig.Rank = ADC_REGULAR_RANK_3;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
@@ -942,7 +945,6 @@ uint16_t ADC_Convert(ADC_HandleTypeDef* adc)
   HAL_ADC_Start(adc);
   HAL_ADC_PollForConversion(adc, HAL_MAX_DELAY);
   uint16_t adcval = HAL_ADC_GetValue(adc);
-  HAL_ADC_Stop(adc);
   return adcval;
 }
 
